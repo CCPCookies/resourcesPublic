b8d564f2fdd73c930cae8e38f6700707c879d52d
James Robert Hawk <gilbert@ccpgames.com>
2026-07-16 12:05:25 +0000

    Remove v141 presets and fix Windows build kt

--- .teamcity/Windows/Project.kt
+++ .teamcity/Windows/Project.kt
@@ -20,16 +20,12 @@ import jetbrains.buildServer.configs.kotlin.triggers.vcs
 import jetbrains.buildServer.configs.kotlin.vcs.GitVcsRoot
 import jetbrains.buildServer.configs.kotlin.buildFeatures.provideAwsCredentials
 
-val Debug = CarbonBuildWindows("Debug Windows", "Debug", "x64-windows-debug")
-val Internal = CarbonBuildWindows("Internal Windows", "Internal", "x64-windows-internal")
-val TrinityDev = CarbonBuildWindows("TrinityDev Windows", "TrinityDev", "x64-windows-trinitydev")
-val Release = CarbonBuildWindows("Release Windows", "Release", "x64-windows-release")
-val ReleaseDevFeatures = CarbonBuildWindows("Release Windows with Dev Features", "Release", "x64-windows-release-with-dev-features")
-
-val Debug_v145 = CarbonBuildWindows("Debug Windows v145", "Debug", "x64-windows-v145-debug", "-arch=x64 -vcvars_ver=14.51")
-val Internal_v145 = CarbonBuildWindows("Internal Windows v145", "Internal", "x64-windows-v145-internal", "-arch=x64 -vcvars_ver=14.51")
-val TrinityDev_v145 = CarbonBuildWindows("TrinityDev Windows v145", "TrinityDev", "x64-windows-v145-trinitydev", "-arch=x64 -vcvars_ver=14.51")
-val Release_v145 = CarbonBuildWindows("Release Windows v145", "Release", "x64-windows-v145-release", "-arch=x64 -vcvars_ver=14.51")
+val BuildParameters = "-arch=x64 -vcvars_ver=14.51"
+val Debug = CarbonBuildWindows("Debug Windows", "Debug", "x64-windows-debug",BuildParameters)
+val Internal = CarbonBuildWindows("Internal Windows", "Internal", "x64-windows-internal",BuildParameters)
+val TrinityDev = CarbonBuildWindows("TrinityDev Windows", "TrinityDev", "x64-windows-trinitydev",BuildParameters)
+val Release = CarbonBuildWindows("Release Windows", "Release", "x64-windows-release",BuildParameters)
+val ReleaseDevFeatures = CarbonBuildWindows("Release Windows with Dev Features", "Release", "x64-windows-release-with-dev-features",BuildParameters)
 
 object Project : Project({
     id("Windows")
@@ -40,16 +36,10 @@ object Project : Project({
     buildType(TrinityDev)
     buildType(Release)
     buildType(ReleaseDevFeatures)
-
-    buildType(Debug_v145)
-    buildType(Internal_v145)
-    buildType(TrinityDev_v145)
-    buildType(Release_v145)
-    buildType(ReleaseDevFeatures_v145)
 })
 
 
-class CarbonBuildWindows(buildName: String, configType: String, preset: String) : BuildType({
+class CarbonBuildWindows(buildName: String, configType: String, preset: String,  vsDevBatSwitches: String) : BuildType({
     id(buildName.toId())
     this.name = buildName
 
@@ -59,8 +49,7 @@ class CarbonBuildWindows(buildName: String, configType: String, preset: String) 
         param("env.GIT_TAG_HASH_OVERRIDE", "")
         param("github_checkout_folder", "github")
         param("env.CTEST_JUNIT_OUTPUT_FILE", "ctest_results.xml")
-        select("env.VISUAL_STUDIO_PLATFORM_TOOLSET", "v141", label = "Visual Studio Platform Toolset", description = "Specify the toolset for the build. e.g. v141 or v143.",
-                options = listOf("v141 (2017)" to "v141", "v143 (2022)" to "v143"))
+        param("VS_DEV_BAT_SWITCHES", vsDevBatSwitches)
         param("env.CMAKE_BUILD_TARGETS", "all")
         param("env.CMAKE_INSTALL_PREFIX", ".build-artifact")
         param("env.CMAKE_CONFIG_TYPE", configType)
@@ -102,7 +91,7 @@ class CarbonBuildWindows(buildName: String, configType: String, preset: String) 
             scriptContent = """
                 REM unfortunately ninja does not find the VS environment otherwise
                 REM NB: the exported PATH also contains the location where we installed sentry-cli, e.g. teamcity.agent.work.dir
-                call "%%ProgramFiles(x86)%%\Microsoft Visual Studio\2017\BuildTools\Common7\Tools\vsdevcmd.bat" -arch=x64
+                call "%env.VSDEV_BAT_PATH%" %VS_DEV_BAT_SWITCHES%
                 echo ##teamcity[setParameter name='env.INCLUDE' value='%%INCLUDE%%']
                 echo ##teamcity[setParameter name='env.LIB' value='%%LIB%%']
                 echo ##teamcity[setParameter name='env.LIBPATH' value='%%LIBPATH%%']
